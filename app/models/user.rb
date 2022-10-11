a2e1917f850c974529d365cb12fca6b110b1306f
Katharine Rawson Sibbald <katharine.sibbald@xplortechnologies.com>
2022-10-11 15:25:28 -0400

    Refactor seeds
    Make a list of users in the user model
    so it can be used by user recreator later

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,29 @@
 class User < ApplicationRecord
+
+  SEED_USER_ARRAY =
+  [
+    {
+      first_name: "Katharine",
+      last_name: "Sibbald",
+      email: 'katharine.sibbald@xplortechnologies.com'
+    },
+    {
+      first_name: 'Katharine',
+      last_name: "should be false",
+      email: 'should.be.false@xplortechnologies.com'
+    },
+    {
+      first_name: 'Katharine',
+      last_name: "should be true",
+      email: 'should.be.true@xplortechnologies.com'
+    },
+    {
+      first_name: 'Katharine',
+      last_name: "change after app starts",
+      email: 'change.after.app.starts@xplortechnologies.com'
+    }
+  ]
+
   def ask_config_cat_for_key
     ConfigCatClient.get_value(
       'BUG_TRACKING',
