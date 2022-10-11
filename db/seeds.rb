8e981648d07c62864db73ecdf70eaaccfc845a3c
Katharine Rawson Sibbald <katharine.sibbald@xplortechnologies.com>
2022-10-11 14:15:55 -0400

    refactor seed file and add user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.find_or_create_by(
-  first_name: "Katharine",
-  last_name: "Sibbald",
-  email: 'katharine.sibbald@xplortechnologies.com'
-)
+SEED_USER_ARRAY =
+[
+  {
+    first_name: "Katharine",
+    last_name: "Sibbald",
+    email: 'katharine.sibbald@xplortechnologies.com'
+  },
+  {
+    first_name: 'Katharine',
+    last_name: "should be false",
+    email: 'should.be.false@xplortechnologies.com'
+  },
+  {
+    first_name: 'Katharine',
+    last_name: "should be true",
+    email: 'should.be.true@xplortechnologies.com'
+  },
+  {
+    first_name: 'Katharine',
+    last_name: "change after app starts",
+    email: 'change.after.app.starts@xplortechnologies.com'
+  }
+]
 
-User.find_or_create_by(
-  first_name: 'Katharine',
-  last_name: "should be false",
-  email: 'should.be.false@xplortechnologies.com'
-)
-
-User.find_or_create_by(
-  first_name: 'Katharine',
-  last_name: "should be true",
-  email: 'should.be.true@xplortechnologies.com'
-)
+SEED_USER_ARRAY.each do |user_hash|
+  User.find_or_create_by( user_hash )
+end
